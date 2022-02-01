1c4a44caeada73ac292c9600942c41471d51597a
Jefte <jefte199@gmail.com>
2022-01-31 22:49:18 -0300

    feat: create page person

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,12 +1,31 @@
+import axios from 'axios';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import style from './Persons.module.css';
 
 export default function Persons() {
   const { id } = useParams();
+//  const baseURL = "https://rickandmortyapi.com/api/character";
+  const [resultApi, setResultApi] = React.useState("https://rickandmortyapi.com/api/character/1");
 
+  React.useEffect(async () => {
+    await axios.get(`https://rickandmortyapi.com/api/character/${id}`).then((response) => {
+      setResultApi(response.data.results);
+    });
+  }, []);
+
+  console.log(resultApi);
   return (
     <>
-      <h1>{console.log(id)}</h1>
+      <div className={style.row}>
+        <div className={style.leftcolumn}>
+          <div className={style.card}>
+            <h2>Name</h2>
+            <h5>Title description, Dec 7, 2017</h5>
+            <p>Some text..</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/RickSearch.js
+++ src/components/RickSearch.js
@@ -5,34 +5,28 @@ import { Link } from 'react-router-dom';
 
 export default function RickSearch() {
   const [valueInput, setValueInput] = React.useState(null);
-  const [result, setResult] = React.useState(null);
+  const [resultApi, setResultApi] = React.useState(null);
+  const [load, setLoad] = React.useState(false);
 
   const baseURL = "https://rickandmortyapi.com/api/character";
 
-  function api() {
-    axios.get(`${baseURL}/?name=${valueInput}`).then((response) => {
-      setResult(response.data.results);
+  async function api() {
+    setLoad(true);
+    await axios.get(`${baseURL}/?name=${valueInput}`).then((response) => {
+      setResultApi(response.data.results);
     });
+    setLoad(false);
   }
 
   return (
     <>
-      <h1 className={style.containerText}>Rick web</h1>
-      <div className={style.superinfoBg}>
-        <div className={style.superinfo}>
-          <p>Personagens: 826</p>
-          <p>Locais: 126</p>
-          <p>Episodios: 51</p>
-        </div>
-      </div>
-
       <div className={style.containerSearch}>
         <input type="text" value={valueInput} onChange={(event) => setValueInput(event.target.value)} placeholder="exemplo: Rick" className={style.search} />
         <button onClick={api} className={style.button}>Buscar</button>
       </div>
 
       <div className={style.container}>
-        {result ? result.map((item) => {
+        {resultApi ? resultApi.map((item) => {
           return (
             <div className={style.space} key={item.id}>
               <Link to={`person/${item.id}`} className={style.link}>
@@ -40,12 +34,17 @@ export default function RickSearch() {
                 <div>
                   <h4><b>{item.name}</b></h4>
                 </div>
+                <button className={item.status == 'Alive' ? style.statusL : style.statusD}>{item.status}</button>
               </Link>
-              <h1 className={style.containerText}>{item.nome}</h1>
+              <h1 className={style.status}>{item.nome}</h1>
             </div>
           );
-        }) : <h1 className={style.containerText}>Esperando pesquisa</h1>}
+        }) : <h1 className={style.containerText}>Esperando pesquisa {load ? "Carregando....." : null}</h1>}
       </div>
+      <div className={style.footer}>
+        <p>Apenas 20 respostas por pesquisa</p>
+      </div>
+
     </>
   );
 }
